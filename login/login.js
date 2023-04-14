6a7fc1d501e7931c4355bf971e9885f435110a51
Brayan Caldera <bryancvra07@gmail.com>
2023-04-14 14:45:16 -0400

    V1.0

---
+++ login/login.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', subtitulo)
+
+// variables
+
+let subtitle = document.querySelector('.subtitle')
+
+// funciones principales
+
+function subtitulo(){
+subtitle.innerHTML = 'Ingresa a tu cuenta para acceder a las ultimas novedades';
+}
+
+// funciones secundarias 
