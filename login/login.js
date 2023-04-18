01e0ca6e1f4acadc272e26480192936754bfe141
Brayan Caldera <bryancvra07@gmail.com>
2023-04-18 17:28:06 -0400

    V1.6

--- login/login.js
+++ login/login.js
@@ -1,8 +1,10 @@
 window.addEventListener('load', subtitulo)
-
 let subtitle = document.querySelector('.subtitle')
+function subtitulo(){
+    subtitle.innerHTML = 'Ingresa a tu cuenta para acceder a las ultimas novedades';
+}
 
-
+// sing out desktop seleccionando correo
 const email = document.querySelector('.email')
 const desktopMenu = document.querySelector('.desktop-menu');
 const login = document.querySelector('.login');
@@ -11,22 +13,28 @@ const footer = document.querySelector('.footer');
 
 email.addEventListener('click', toggleDesktopMenu)
 function toggleDesktopMenu () {
-    desktopMenu.classList.remove('inactive');
+    desktopMenu.classList.toggle('inactive');
 }
 
 login.addEventListener('click', clickonBody);
 footer.addEventListener('click', clickonBody);
+
 function clickonBody() {
-    desktopMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+}
+
+// sign out mobile
+
+const logoMnu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+logoMnu.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu (){
+    mobileMenu.classList.toggle('inactive')
 }
 
 
-// funciones principales
-
-function subtitulo(){
-subtitle.innerHTML = 'Ingresa a tu cuenta para acceder a las ultimas novedades';
-}
 
 
-
-// funciones secundarias 
--- readme.md
+++ readme.md
@@ -14,5 +14,9 @@ Se modifico el footer añadiendole un boton que lleva al principio de la pagina,
 git reset --hard db4f5256a9a84c5d3edfa35b1b4d24db5930eb5e
 
 V1.4: Se agrego un archivo de estilos que almacena los estilos del navbar y del footer
+git reset --hard 12eeed8a4f5f104ec74157629029d06bbb7eaacd
 
 V1.5: Se agrego un menu de opciones en el email del navbar.
+git reset --hard d99b1366285ac5bca494efc797011a11a7a73a48
+
+V1.6: Se agrego el menu para que se despliegue en mobile.
