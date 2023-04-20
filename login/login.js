839d398925f0a7be410f5d78b31b8451af9f38f9
Brayan Caldera <bryancvra07@gmail.com>
2023-04-20 15:36:19 -0400

    V1.6.7

--- login/login.js
+++ login/login.js
@@ -4,36 +4,7 @@ function subtitulo(){
     subtitle.innerHTML = 'Ingresa a tu cuenta para acceder a las ultimas novedades';
 }
 
-// sing out desktop seleccionando correo
-const email = document.querySelector('.email')
-const desktopMenu = document.querySelector('.desktop-menu');
-const login = document.querySelector('.login');
-const footer = document.querySelector('.footer');
 
-
-email.addEventListener('click', toggleDesktopMenu)
-function toggleDesktopMenu () {
-    desktopMenu.classList.toggle('inactive');
-}
-
-login.addEventListener('click', clickonBody);
-footer.addEventListener('click', clickonBody);
-
-function clickonBody() {
-    desktopMenu.classList.add('inactive');
-    mobileMenu.classList.add('inactive');
-}
-
-// sign out mobile
-
-const logoMnu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-logoMnu.addEventListener('click', toggleMobileMenu)
-
-function toggleMobileMenu (){
-    mobileMenu.classList.toggle('inactive')
-}
 
 
 
