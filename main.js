0e3e40f3f3b8cb8b29dd95f65aefe9849f417f5c
Brayan Caldera <bryancvra07@gmail.com>
2023-04-20 15:38:55 -0400

    V1.6.8

---
+++ main.js
@@ -0,0 +1,29 @@
+// sing out desktop seleccionando correo
+const email = document.querySelector('.email')
+const desktopMenu = document.querySelector('.desktop-menu');
+const login = document.querySelector('.login');
+const footer = document.querySelector('.footer');
+
+
+email.addEventListener('click', toggleDesktopMenu)
+function toggleDesktopMenu () {
+    desktopMenu.classList.toggle('inactive');
+}
+login.addEventListener('click', clickonBody);
+footer.addEventListener('click', clickonBody);
+
+function clickonBody() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+}
+
+// sign out mobile
+
+const logoMnu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+logoMnu.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu (){
+    mobileMenu.classList.toggle('inactive')
+}
