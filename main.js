073feeb14b7fa0c439b6d9cc66c381f8b496f2af
Brayan Caldera <bryancvra07@gmail.com>
2023-04-20 21:18:36 -0400

    V1.6.9.1

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-// sing out desktop seleccionando correo
 const email = document.querySelector('.email')
 const desktopMenu = document.querySelector('.desktop-menu');
 const login = document.querySelector('.login');
 const footer = document.querySelector('.footer');
 
 
+// sing out desktop seleccionando correo
+
 email.addEventListener('click', toggleDesktopMenu)
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
 }
+
 login.addEventListener('click', clickonBody);
 footer.addEventListener('click', clickonBody);
 
 function clickonBody() {
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
+    productDetail.classList.add('inactive');
 }
 
 // sign out mobile
@@ -26,4 +29,15 @@ logoMnu.addEventListener('click', toggleMobileMenu)
 
 function toggleMobileMenu (){
     mobileMenu.classList.toggle('inactive')
+}
+
+// aside carrito de compras
+
+const productDetail = document.querySelector('.product-detail');
+const shoppingCart = document.querySelector('.cart');
+
+shoppingCart.addEventListener('click', toggleShoppingCart)
+
+function toggleShoppingCart (){
+    productDetail.classList.toggle('inactive');
 }
