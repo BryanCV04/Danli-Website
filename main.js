292609a88391ff3e91c99a9550468a239ce78966
Brayan Caldera <bryancvra07@gmail.com>
2023-04-24 16:39:19 -0400

    V2.0

--- main.js
+++ main.js
@@ -72,3 +72,118 @@ shoppingCart.addEventListener('click', desactivarMenu)
 function desactivarMenu (){
     mobileMenu.classList.add('inactive')
 }
+
+// servicios precios e informacion
+
+let productList = [];
+
+const cardContainer = document.querySelector('.card-container');
+
+productList.push({
+    name: 'Auditoria de la Marca',
+    price: 'Ref. $40',
+    img: 'https://th.bing.com/th/id/OIG.CiJ8MZNbQqzN4wYils85?pcl=1b1a19&pid=ImgGn'
+});
+productList.push({
+    name: 'Asesoria Presencial',
+    price: 'Ref. $30',
+    img: 'https://th.bing.com/th/id/OIG.vqSkRQA1CfmwvFVbRJMe?w=173&h=173&c=6&pcl=1b1a19&r=0&o=5&pid=ImgGn'
+});
+productList.push({
+    name: 'Asesoria Virtual',
+    price: 'Ref. $30',
+    img: 'https://th.bing.com/th/id/OIG.tR4OyriYJlEGMlNmWMLN?w=173&h=173&c=6&pcl=1b1a19&r=0&o=5&pid=ImgGn'
+});
+productList.push({
+    name: 'Gestion de Ads',
+    price: 'Ref. $50',
+    img: 'https://th.bing.com/th/id/OIG.0Lo1_W7O0zLHRraZuGxl?w=173&h=173&c=6&pcl=1b1a19&r=0&o=5&pid=ImgGn'
+});
+productList.push({
+    name: 'Desbloqueo de Cuentas Publicitarias',
+    price: 'Ref. $100',
+    img: 'https://th.bing.com/th/id/OIG.JKzygCvz5bUbsE_Lcosn?w=173&h=173&c=6&pcl=1b1a19&r=0&o=5&pid=ImgGn'
+});
+productList.push({
+    name: 'Creacion de Logo',
+    price: 'Ref. $100',
+    img: 'https://th.bing.com/th/id/OIG.X7sfMDO0RZb6Kl4p.Sb8?w=173&h=173&c=6&pcl=1b1a19&r=0&o=5&pid=ImgGn'
+});
+productList.push({
+    name: 'Creacion de Logo + Branding',
+    price: 'Ref. $150',
+    img: 'https://th.bing.com/th/id/OIG.mZP7Spb8ufiCGMu1IZu1?pcl=1b1a19&pid=ImgGn'
+});
+productList.push({
+    name: 'Soporte de Diseño para Redes Sociales',
+    price: 'Ref. $80',
+    img: 'https://th.bing.com/th/id/OIG.LaAYuSQQZOkE1M3M9Bxl?pcl=1b1a19&pid=ImgGn'
+});
+productList.push({
+    name: 'Diseño de Catálogo',
+    price: 'Ref. $1',
+    img: 'https://th.bing.com/th/id/OIG.GWApO.w3cspPFOxaqKxl?w=173&h=173&c=6&pcl=1b1a19&r=0&o=5&pid=ImgGn'
+});
+productList.push({
+    name: 'Creación De Landing Page',
+    price: 'Ref. $300',
+    img: 'https://th.bing.com/th/id/OIG.5EEjo_eZgrZ6qShQzz_1?w=173&h=173&c=6&pcl=1b1a19&r=0&o=5&pid=ImgGn'
+});
+productList.push({
+    name: 'Creación De Aplicacion Web',
+    price: 'Ref. $600',
+    img: 'https://th.bing.com/th/id/OIG.yNKfeb.4_bXlGJM0YHSP?w=173&h=173&c=6&pcl=1b1a19&r=0&o=5&pid=ImgGn'
+});
+productList.push({
+    name: 'Email Marketing',
+    price: 'No Avaible',
+    img: 'https://th.bing.com/th/id/OIG.dCpwuWFRelWu8FMXXAIj?w=173&h=173&c=6&pcl=1b1a19&r=0&o=5&pid=ImgGn'
+});
+
+function renderServices (arr){
+    for (product of arr){
+        let productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        let img = document.createElement('img');
+        img.setAttribute('src', product.img);
+    
+        let productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        let productInfoDiv = document.createElement('div');
+    
+        let productName = document.createElement('p');
+        productName.innerText = product.name
+    
+        let productPrice = document.createElement('p');
+        productPrice.innerText = product.price;
+    
+        productInfoDiv.appendChild(productName);
+        productInfoDiv.appendChild(productPrice);
+    
+        let productInfoFigure = document.createElement('figure');
+        let ProductImgPLUS = document.createElement('img');
+        ProductImgPLUS.setAttribute('src', '../assets/iconos/icono+svg-cropped.svg');
+    
+        productInfoFigure.appendChild(ProductImgPLUS);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(img);
+        productCard.appendChild(productInfo);
+    
+        // aqui se lo insertamos al form container
+        cardContainer.appendChild(productCard);
+    }
+}
+renderServices(productList);
+
+
+
+
+
+
+
+
