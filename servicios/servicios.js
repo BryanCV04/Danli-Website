2b08aba70d30035db6631b76fe6264ad29f67899
Brayan Caldera <bryancvra07@gmail.com>
2023-05-25 23:11:55 -0400

    V2.3

---
+++ servicios/servicios.js
@@ -0,0 +1,71 @@
+const btnWhatsapp1 = document.querySelector('.btn01');
+btnWhatsapp1.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Auditoria%20de%20la%20marca.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp2 = document.querySelector('.btn02');
+btnWhatsapp2.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Asesoria%20presencial.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp3 = document.querySelector('.btn03');
+btnWhatsapp3.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Asesoria%20virtual.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp4 = document.querySelector('.btn04');
+btnWhatsapp4.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Gestion%20de%20ads.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp5 = document.querySelector('.btn05');
+btnWhatsapp5.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Desbloqueo%20de%20cuentas%20publicitarias.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp6 = document.querySelector('.btn06');
+btnWhatsapp6.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Creacion%20de%20logo.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp7 = document.querySelector('.btn07');
+btnWhatsapp7.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Creacion%20de%20logo%20Y%20branding`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp8 = document.querySelector('.btn08');
+btnWhatsapp8.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Soporte%20de%20diseño%20para%20redes%20sociales.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp9 = document.querySelector('.btn09');
+btnWhatsapp9.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Diseño%20de%20catalogo%20de%20productos.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp10 = document.querySelector('.btn10');
+btnWhatsapp10.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Creacion%20de%20Landing%20Page.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp11 = document.querySelector('.btn11');
+btnWhatsapp11.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Creacion%20de%20Aplicacion%20Web.`;
+    window.open(url, '_blank');
+});
+
+const btnWhatsapp12 = document.querySelector('.btn12');
+btnWhatsapp12.addEventListener('click', () => {
+    const url = `https://api.whatsapp.com/send?phone=584127380044&text=https://api.whatsapp.com/send?phone=584127380044&text=Saludos!%20quiero%20saber%20mas%20sobre%20sus%20servicios%20de%20Email%20marketing.`;
+    window.open(url, '_blank');
+});
